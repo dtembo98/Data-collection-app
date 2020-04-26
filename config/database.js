820b2aabec02b6ed558f65cc515364a80fbca33f
trojan <trojan@localhost.localdomain>
2020-04-26 14:07:52 +0200

    compression middlware,winston logger

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
-  "pass104",
+  process.env.DB_PASSWORD,
   {
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
